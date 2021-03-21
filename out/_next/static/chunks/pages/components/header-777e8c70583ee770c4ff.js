4aa2359bdd03fcafa9c0a44ce8e5a13e4a510cc8
unknown <jxc1129@yandex.com>
2021-03-22 01:32:06 +1000

    commit

--- out/_next/static/chunks/pages/components/header-777e8c70583ee770c4ff.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[7],{vGQ4:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/header",function(){return o("Isfp")}])}},[["vGQ4",0,2,1,3]]]);
--- out/_next/static/chunks/pages/index-9cf2abcc415b89f491c9.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[8],{RNiq:function(n,t,u){"use strict";u.r(t),u.d(t,"default",(function(){return o}));var i=u("nKUr"),_=u("Isfp");function o(){return Object(i.jsx)(_.default,{})}},vlRD:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u("RNiq")}])}},[["vlRD",0,2,1,3]]]);
